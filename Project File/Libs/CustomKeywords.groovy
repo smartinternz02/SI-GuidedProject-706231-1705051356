5761da719d0d80188cb7f5ddbca98149e789fc13
priyamore18 <140368003+priyamore18@users.noreply.github.com>
2024-01-30 09:28:19 +0530

    Add files via upload

---
+++ Project File/Libs/CustomKeywords.groovy
@@ -0,0 +1,10 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+
+
+def static "custom_key.keyword.addcard"() {
+    (new custom_key.keyword()).addcard()
+}
